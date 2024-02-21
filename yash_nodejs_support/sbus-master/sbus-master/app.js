d374d3b3e1207e593703df72a4e0a6d8813ac9f8
Karthi <karthi@dronestark.com>
2024-02-21 17:34:35 +0530

    Added universal joystick support

--- yash_nodejs_support/sbus-master/sbus-master/app.js
+++ yash_nodejs_support/sbus-master/sbus-master/app.js
@@ -34,7 +34,7 @@ var port = new SerialPort('/dev/ttyUSB0',{
 port.pipe(parser);
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, '/templates/joy2.html'));
+    res.sendFile(join(__dirname, '/templates/joy3.html'));
     // res.sendFile(join(__dirname, '/static/joy2.js'));
 });
 
